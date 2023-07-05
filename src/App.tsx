3c93ebfb7265755a3ef9798338e6b9cf9628e9a9
root <rootbeer.tiam@gmail.com>
2023-07-06 02:20:12 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface AppProps {
+    name: string;
 }
 
-export default App;
+const Home: React.FC = () => {
+    return <div>Home Page</div>
+}
+
+const About: React.FC = () => {
+    return <div>About Page</div>
+}
+
+const NotFound: React.FC = () => {
+    return <div>404 Not Found</div>
+}
+
+const App: React.FC<AppProps> = ({ name }) => {
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/" component={Home}/>
+                <Route path="/about" component={About}/>
+                <Route component={NotFound}/>
+            </Switch>
+        </Router>
+    )
+}
+
+export default App
